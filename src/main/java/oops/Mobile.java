499fbb375ab9d5facd2739dbc1d39ca18773dfcc
Revanthkumarr <revanthkumarr15@gmail.com>
2024-01-31 15:55:50 -0600

    oops

---
+++ src/main/java/oops/Mobile.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class Mobile {
+
+    private static int price =0;
+
+    public static int getPrice(){
+        return price;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Price: " + Mobile.getPrice());
+    }
+}
