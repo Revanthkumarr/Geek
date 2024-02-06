2ff7d7308cac8146f958b08bd5bcf6689e0f6f31
Revanthkumarr <revanthkumarr15@gmail.com>
2024-02-06 12:56:04 -0600

    Lambda Alpha

---
+++ src/main/java/lambda/Alphastr.java
@@ -0,0 +1,30 @@
+package lambda;
+
+import java.util.*;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+
+public class Alphastr {
+    public static void main(String[] args) {
+        List<String> s= Arrays.asList("sam","ram","shiva","alpha");
+       // s.forEach(System.out::println);
+
+
+        List<String> capitalizedList = s.stream()
+                .map(str -> Character.toUpperCase(str.charAt(0)) + str.substring(1))
+                .sorted()
+                .toList();
+
+
+
+
+
+
+
+
+        System.out.println(capitalizedList);
+
+    }
+}
